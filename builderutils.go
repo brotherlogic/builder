abb298f10c1a03f0d24778bd1621267f1ed22a2d
Simon Tucker <brotherlogic@gmail.com>
2022-05-12 20:32:44 -0700

    Include the server in the error

--- builderutils.go
+++ builderutils.go
@@ -20,24 +20,24 @@ func (s *Server) runBuild(ctx context.Context, gha string) error {
 
 	out1, err := exec.Command("git", "clone", gha, "./").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("%v -> %v", err, string(out1))
+		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out1))
 	}
 
 	out2, err := exec.Command("go", "get", "-u", "./...").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("%v -> %v", err, string(out2))
+		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out2))
 	}
 
 	out3, err := exec.Command("go", "mod", "tidy").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("%v -> %v", err, string(out3))
+		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out3))
 	}
 
 	out4, err1 := exec.Command("git", "push", "origin", "main").CombinedOutput()
 	out5, err2 := exec.Command("git", "push", "origin", "master").CombinedOutput()
 
 	if err1 != nil && err2 != nil {
-		return fmt.Errorf("Unable to push: %v or %v -> %v, %v", err1, err2, out4, out5)
+		return fmt.Errorf("(%v) Unable to push: %v or %v -> %v, %v", s.Registry.Identifier, err1, err2, out4, out5)
 	}
 
 	return nil
