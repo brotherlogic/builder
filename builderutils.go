3aa493901429db09435c0ff4836b24b1949f85cd
Simon Tucker <brotherlogic@gmail.com>
2022-06-23 17:46:35 -0700

    Add conditions to the errors. This closes #19

--- builderutils.go
+++ builderutils.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -20,29 +21,29 @@ func (s *Server) runBuild(ctx context.Context, gha string) error {
 
 	out1, err := exec.Command("git", "clone", gha, "./").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out1))
+		return status.Errorf(codes.FailedPrecondition, "(%v) %v -> %v", s.Registry.Identifier, err, string(out1))
 	}
 
 	out2, err := exec.Command("go", "get", "-u", "./...").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out2))
+		return status.Errorf(codes.FailedPrecondition, "(%v) %v -> %v", s.Registry.Identifier, err, string(out2))
 	}
 
 	out3, err := exec.Command("go", "mod", "tidy").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out3))
+		return status.Errorf(codes.FailedPrecondition, "(%v) %v -> %v", s.Registry.Identifier, err, string(out3))
 	}
 
 	out6, err := exec.Command("git", "commit", "-am", "DownstreamUpdates").CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("(%v) %v -> %v", s.Registry.Identifier, err, string(out6))
+		return status.Errorf(codes.FailedPrecondition, "(%v) %v -> %v", s.Registry.Identifier, err, string(out6))
 	}
 
 	out4, err1 := exec.Command("git", "push", "origin", "main").CombinedOutput()
 	out5, err2 := exec.Command("git", "push", "origin", "master").CombinedOutput()
 
 	if err1 != nil && err2 != nil {
-		return fmt.Errorf("(%v) Unable to push: %v or %v -> %v, %v", s.Registry.Identifier, err1, err2, out4, out5)
+		return status.Errorf(codes.FailedPrecondition, "(%v) Unable to push: %v or %v -> %v, %v", s.Registry.Identifier, err1, err2, out4, out5)
 	}
 
 	return nil
