4501e293f76ab013fe693e1b638ca3dd3b11b6f5
Simon Tucker <brotherlogic@gmail.com>
2022-11-28 01:56:17 -0800

    Fixed build issue. This closes #41

--- builderapi.go
+++ builderapi.go
@@ -13,13 +13,13 @@ const (
 	QUEUE = "/github.com/brotherlogic/recordadder/queue"
 )
 
-//AddRecord adds a record into the system
+// AddRecord adds a record into the system
 func (s *Server) Refresh(ctx context.Context, req *pb.RefreshRequest) (*pb.RefreshResponse, error) {
 	s.CtxLog(ctx, fmt.Sprintf("Building for %v", req.GetJob()))
 	err := s.runBuild(ctx, fmt.Sprintf("git@github.com:brotherlogic/%v", req.GetJob()))
 	s.CtxLog(ctx, fmt.Sprintf("Build result: %v", err))
 	if err != nil {
-		s.BounceIssue("Refresh Build Error", fmt.Sprintf("%v", err), req.GetJob())
+		s.BounceIssue(ctx, "Refresh Build Error", fmt.Sprintf("%v", err), req.GetJob())
 	}
 	return &pb.RefreshResponse{}, err
 }
